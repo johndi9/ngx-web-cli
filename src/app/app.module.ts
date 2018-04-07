0ad97809e560b83ee3e6a4f9cd2315a997e95104
Juan Diego <johndi9@hotmail.com>
2018-04-08 01:41:31 +0200

    Set up correctly lazy load for the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
 import { SharedModule } from './components/shared/shared.module';
 
 @NgModule({
@@ -11,9 +13,13 @@ import { SharedModule } from './components/shared/shared.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES, {
+      useHash: Boolean(history.pushState) === false,
+      preloadingStrategy: PreloadAllModules
+    }),
     SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {}
--- src/app/components/pages/home/home.module.ts
+++ src/app/components/pages/home/home.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DynamicHeaderModule } from '../../modules/dynamic-header/dynamic-header.module';
 import { HomeComponent } from './home.component';
+import { ROUTES } from './home.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DynamicHeaderModule,
+    RouterModule.forChild(ROUTES),
   ],
-  declarations: [HomeComponent]
+  declarations: [ HomeComponent ]
 })
-export class HomeModule { }
+export class HomeModule {}
