d2f483773385bcbe22a50df4e19d62465b0328dc
Juan Diego <johndi9@hotmail.com>
2018-04-08 02:03:30 +0200

    Set correctly video url Structure changes Different styles applied

--- src/app/components/modules/dynamic-header/containers/dynamic-header-cont/dynamic-header-cont.component.ts
+++ src/app/components/modules/dynamic-header/containers/dynamic-header-cont/dynamic-header-cont.component.ts
@@ -1,39 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-header-cont',
   templateUrl: './dynamic-header-cont.component.html',
   styleUrls: [ './dynamic-header-cont.component.scss' ]
 })
-export class DynamicHeaderContComponent implements OnInit, OnDestroy {
-  private videoPath: string;
-  private subscription: Subscription;
+export class DynamicHeaderContComponent {
+  readonly videoPath = '/assets/videos/bg-min.mp4';
 
   constructor() {}
-
-  ngOnInit(): void {
-    this.subscription = this.loadVideo().subscribe(() => {
-      this.videoPath = '/assets/video/bg-min.mp4';
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  private loadVideo(): Observable<HTMLVideoElement> {
-    return Observable.create(observer => {
-      const video: HTMLVideoElement = document.createElement('video');
-
-      video.src = this.videoPath;
-      video.load();
-      video.addEventListener('loadeddata', () => {
-        observer.next();
-        observer.complete();
-      });
-      video.onerror = err => observer.error(err);
-    });
-  }
 }
