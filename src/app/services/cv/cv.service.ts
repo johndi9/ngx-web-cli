21dc425e7daedaf52206308739eb0f31fce5380e
Juan Diego <johndi9@hotmail.com>
2018-04-08 18:29:54 +0200

    Set API for CV service

--- src/app/services/cv/cv.service.ts
+++ src/app/services/cv/cv.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { CurriculumInt } from '../../interfaces/cv/curriculum.int';
+import { XhrStateInt } from '../../interfaces/xhr-state/xhr-state.int';
+import { State } from '../../state';
+import { LoadCV } from '../../state/cv/cv.actions';
 
 @Injectable()
 export class CvService {
+  constructor(private store: Store<State>) {
+    this._cv$ = store.select('cv');
+  }
 
-  constructor() { }
+  private _cv$: Observable<XhrStateInt<CurriculumInt>>;
 
+  get cv$(): Observable<XhrStateInt<CurriculumInt>> {
+    return this._cv$;
+  }
+
+  loadCurriculum() {
+    this.store.dispatch(new LoadCV());
+  }
 }
