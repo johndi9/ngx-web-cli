184e9467a2ced770527d6d34e1565263357d47b4
Juan Diego <johndi9@hotmail.com>
2018-04-08 21:04:24 +0200

    Connected sticky header with home page

--- src/app/components/pages/home/home.module.ts
+++ src/app/components/pages/home/home.module.ts
@@ -5,17 +5,19 @@ import { CvModule } from '../../../services/cv/cv.module';
 import { ModalModule } from '../../../services/modal/modal.module';
 import { TabModule } from '../../../services/tab/tab.module';
 import { DynamicHeaderModule } from '../../modules/dynamic-header/dynamic-header.module';
+import { StickySidebarModule } from '../../modules/sticky-sidebar/sticky-sidebar.module';
 import { HomeComponent } from './home.component';
 import { ROUTES } from './home.routes';
 
 @NgModule({
   imports: [
     CommonModule,
-    DynamicHeaderModule,
     RouterModule.forChild(ROUTES),
     CvModule,
     TabModule,
     ModalModule,
+    DynamicHeaderModule,
+    StickySidebarModule,
   ],
   declarations: [ HomeComponent ]
 })
