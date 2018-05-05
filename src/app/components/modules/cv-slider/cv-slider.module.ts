49465f27ee1c7747826efb0d3f2930fd753baf3d
Juan Diego <johndi9@hotmail.com>
2018-05-05 20:58:39 +0200

    Connect ngx-swiper-wrapper in the slider presenter

--- src/app/components/modules/cv-slider/cv-slider.module.ts
+++ src/app/components/modules/cv-slider/cv-slider.module.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SWIPER_CONFIG, SwiperConfigInterface, SwiperModule } from 'ngx-swiper-wrapper';
 import { CvSliderContComponent } from './containers/cv-slider-cont/cv-slider-cont.component';
 import { CvSliderPresComponent } from './presenters/cv-slider-pres/cv-slider-pres.component';
 
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SwiperModule,
   ],
   declarations: [ CvSliderContComponent, CvSliderPresComponent ],
-  exports: [ CvSliderContComponent ]
+  exports: [ CvSliderContComponent ],
+  providers: [
+    { provide: SWIPER_CONFIG, useValue: DEFAULT_SWIPER_CONFIG }
+  ]
 })
 export class CvSliderModule {}
