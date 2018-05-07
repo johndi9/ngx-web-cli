73c7fbaf9ea6dc432883b7a8f630fdea467aed04
Juan Diego <johndi9@hotmail.com>
2018-05-07 09:47:15 +0200

    Set contact section

--- src/app/components/modules/cv-sections/contact/contact.module.ts
+++ src/app/components/modules/cv-sections/contact/contact.module.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { GoogleMapsModule } from '../../../../services/google-maps/google-maps.module';
+import { SharedModule } from '../../../shared/shared.module';
 import { ContactContComponent } from './containers/contact-cont/contact-cont.component';
 import { ContactPresComponent } from './presenters/contact-pres/contact-pres.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    GoogleMapsModule,
   ],
   declarations: [ ContactContComponent, ContactPresComponent ],
   exports: [ ContactContComponent ]
--- src/app/components/modules/cv-sections/contact/presenters/contact-pres/contact-pres.component.ts
+++ src/app/components/modules/cv-sections/contact/presenters/contact-pres/contact-pres.component.ts
@@ -1,15 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input } from '@angular/core';
 import { ContactInt } from '../../../../../../interfaces/cv/contact/contact.int';
 import { LocationInt } from '../../../../../../interfaces/cv/location/location.int';
 import { SocialInt } from '../../../../../../interfaces/cv/social/social.int';
+import { GoogleMapsService } from '../../../../../../services/google-maps/google-maps.service';
 
 @Component({
   selector: 'app-contact-pres',
   templateUrl: './contact-pres.component.html',
   styleUrls: [ './contact-pres.component.scss' ]
 })
-export class ContactPresComponent {
+export class ContactPresComponent implements AfterViewInit {
   @Input() contact: ContactInt;
   @Input() location: LocationInt;
   @Input() social: SocialInt;
+
+  constructor(private _googleMapsService: GoogleMapsService,
+              private _element: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    (<any>window).googleMapsReady = this.onMapsReady.bind(this);
+    document.getElementsByTagName('head')[ 0 ].appendChild(this.createScriptElement());
+  }
+
+  private createScriptElement(): HTMLScriptElement {
+    return {
+      ...document.createElement('script'),
+      type: 'text/javascript',
+      src: this._googleMapsService.getGoogleMapsUrl()
+    };
+  }
+
+  private onMapsReady() {
+    this._googleMapsService.createMap(this._element.nativeElement.querySelector('.map'),
+      `${this.location.city} ${this.location.country}`);
+  }
 }
