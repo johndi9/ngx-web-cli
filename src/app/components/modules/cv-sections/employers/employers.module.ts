b46343c6f8f7ed2567bf4b86cdf278d7427a8b6d
Juan Diego <johndi9@hotmail.com>
2018-05-07 08:52:47 +0200

    Set employers template

--- src/app/components/modules/cv-sections/employers/employers.module.ts
+++ src/app/components/modules/cv-sections/employers/employers.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
 import { CvCardModule } from '../../cv-card/cv-card.module';
 import { EmployersContComponent } from './containers/employers-cont/employers-cont.component';
 import { EmployersPresComponent } from './presenters/employers-pres/employers-pres.component';
@@ -8,6 +9,7 @@ import { EmployersPresComponent } from './presenters/employers-pres/employers-pr
   imports: [
     CommonModule,
     CvCardModule,
+    SharedModule,
   ],
   declarations: [ EmployersContComponent, EmployersPresComponent ],
   exports: [ EmployersContComponent ]
