df2d805362ffa6f325b3df3bb76c42d6642eb723
Juan Diego <johndi9@hotmail.com>
2018-05-10 00:20:44 +0200

    Added all home routes

--- src/app/components/pages/home/home.routes.ts
+++ src/app/components/pages/home/home.routes.ts
@@ -2,5 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', redirectTo: '/personal', pathMatch: 'full' },
+  { path: 'personal', component: HomeComponent },
+  { path: 'projects', component: HomeComponent },
+  { path: 'employers', component: HomeComponent },
+  { path: 'education', component: HomeComponent },
+  { path: 'other', component: HomeComponent },
+  { path: 'contact', component: HomeComponent }
 ];
