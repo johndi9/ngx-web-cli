74dd426432ed44d60393d5f1fd915e9ffa4a59a2
Juan Diego <johndi9@hotmail.com>
2018-05-12 19:24:21 +0200

    Set initial modal structure

--- src/app/components/modules/cv-modal/cv-modal.module.ts
+++ src/app/components/modules/cv-modal/cv-modal.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 import { CvModalContComponent } from './containers/cv-modal-cont/cv-modal-cont.component';
 import { CvModalPresComponent } from './presenters/cv-modal-pres/cv-modal-pres.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
-  declarations: [CvModalContComponent, CvModalPresComponent]
+  declarations: [ CvModalContComponent, CvModalPresComponent ]
 })
-export class CvModalModule { }
+export class CvModalModule {}
--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { TranslateModule } from '@ngx-translate/core';
@@ -18,6 +19,7 @@ import { ImageModule } from '../elements/image/image.module';
     MatListModule,
     MatChipsModule,
     MatCardModule,
+    MatDialogModule,
   ],
   declarations: [],
 })
