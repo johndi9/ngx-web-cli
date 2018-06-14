bb8dc9830df8c6348534fee6ed828c50cac33221
Juan Diego <johndi9@hotmail.com>
2018-06-15 00:10:48 +0200

    Bind height to be stored in the tab's virtual area (area where it has a sticky position)

--- src/app/components/modules/sticky-sidebar/presenters/sticky-sidebar-small-pres/sticky-sidebar-small-pres.component.ts
+++ src/app/components/modules/sticky-sidebar/presenters/sticky-sidebar-small-pres/sticky-sidebar-small-pres.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { TAB_OPTIONS } from '../../../../../enum/tab/tab-options.enum';
 import { getImage } from '../../../../../helpers/image.helper';
 import { getTabs, getTranslationTabKeys } from '../../../../../helpers/tab.helper';
@@ -12,14 +12,23 @@ import { XhrStateInt } from '../../../../../interfaces/xhr-state/xhr-state.int';
   styleUrls: [ './sticky-sidebar-small-pres.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class StickySidebarSmallPresComponent {
+export class StickySidebarSmallPresComponent implements OnChanges {
   @Output() onTabSelected: EventEmitter<TAB_OPTIONS> = new EventEmitter<TAB_OPTIONS>();
   @Input() cv: XhrStateInt<CurriculumInt>;
   @Input() tabSelected: TabStateInt<TAB_OPTIONS>;
 
+  @HostBinding('style.height.px') height = null;
+
   getImage = getImage;
   getTabs = getTabs;
   getTranslationTabKeys = getTranslationTabKeys;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes[ 'tabSelected' ] && changes[ 'tabSelected' ].currentValue && changes[ 'tabSelected' ].previousValue &&
+      changes[ 'tabSelected' ].currentValue.height !== changes[ 'tabSelected' ].previousValue.height) {
+      this.height = this.tabSelected.height;
+    }
+  }
 
   swipeTab(tab: TAB_OPTIONS) {
     this.onTabSelected.emit(tab);
