747b9ec62433cc29ccf86145bb7bf524f910895c
Juan Diego <johndi9@hotmail.com>
2018-07-15 02:00:53 +0200

    Fixed app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 import { ROUTES } from './app.routes';
@@ -8,7 +9,7 @@ import { ROUTES } from './app.routes';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ TranslateModule.forRoot({}), RouterModule.forRoot(ROUTES) ],
+      imports: [ TranslateModule.forRoot({}), RouterModule.forRoot(ROUTES), ServiceWorkerModule.register('', {enabled: false}) ],
       providers: [ { provide: APP_BASE_HREF, useValue: '/' } ],
       declarations: [
         AppComponent
