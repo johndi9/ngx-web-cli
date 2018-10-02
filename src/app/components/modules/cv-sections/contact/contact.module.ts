5b1d3b8ec253225bdba4a85f4a83c16713e2b47a
Juan Diego <johndi9@hotmail.com>
2018-10-02 20:21:37 +0200

    Added library to support inline svgs

--- src/app/components/modules/cv-sections/contact/contact.module.ts
+++ src/app/components/modules/cv-sections/contact/contact.module.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { InlineSVGModule } from 'ng-inline-svg';
 import { SharedModule } from '../../../shared/shared.module';
 import { GoogleMapModule } from '../../google-map/google-map.module';
 import { ContactContComponent } from './containers/contact-cont/contact-cont.component';
 import { ContactPresComponent } from './presenters/contact-pres/contact-pres.component';
 
-
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    GoogleMapModule
+    GoogleMapModule,
+    InlineSVGModule.forRoot()
   ],
   declarations: [ ContactContComponent, ContactPresComponent ],
   exports: [ ContactContComponent ]
