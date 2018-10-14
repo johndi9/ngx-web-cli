2326610a22412f7ec3978975fab58dcea3c7f165
Juan Diego <johndi9@hotmail.com>
2018-10-14 10:11:32 +0200

    Test no redirection on init

--- src/app/components/pages/home/home.routes.ts
+++ src/app/components/pages/home/home.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 export const ROUTES: Routes = [
-  { path: '', redirectTo: '/personal', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'personal', component: HomeComponent },
   { path: 'projects', component: HomeComponent },
   { path: 'projects/:id', component: HomeComponent },
